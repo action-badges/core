911987ac6f1779a44dc54d4347bc0c7b72786bb0
chris48s <git@chris-shaw.dev>
2026-03-30 20:57:32 +0100

    migrate mocha/c8/esmock/sinon to node:test

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,34 +1,33 @@
 import globals from "globals";
 import js from "@eslint/js";
+import nodeCoreTestPlugin from "eslint-plugin-node-core-test";
 import prettierConfig from "eslint-config-prettier";
 import prettierPlugin from "eslint-plugin-prettier";
-import mochaPlugin from "eslint-plugin-mocha";
 
 const config = [
   js.configs.recommended,
-  mochaPlugin.configs.recommended,
+  nodeCoreTestPlugin.configs.recommended,
   prettierConfig,
   {
     ignores: ["dist"],
   },
   {
     plugins: {
-      mocha: mochaPlugin,
+      "node-core-test": nodeCoreTestPlugin,
       prettier: prettierPlugin,
     },
     languageOptions: {
       ecmaVersion: 2022,
       sourceType: "module",
       globals: {
-        mocha: true,
         ...globals.node,
       },
     },
     rules: {
       "prettier/prettier": ["error"],
-      "mocha/no-pending-tests": ["error"],
-      "mocha/no-exclusive-tests": ["error"],
-      "mocha/max-top-level-suites": ["off"],
+      "node-core-test/no-exclusive-tests": ["error"],
+      "node-core-test/no-incomplete-tests": ["error"],
+      "node-core-test/no-skipped-tests": ["error"],
       "no-unused-vars": ["error", { args: "none" }],
     },
   },
